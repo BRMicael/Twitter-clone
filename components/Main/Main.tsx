320671fdff5cb6395f2d2eb8f544168b0a61b765
Micael Batista <micaelbatista0108@hotmail.com>
2022-03-26 00:40:41 -0300

    finishing the main

---
+++ components/Main/Main.tsx
@@ -0,0 +1,41 @@
+import {
+  ArrowLeftIcon,
+  BellIcon,
+  HomeIcon,
+  MailIcon,
+  SearchIcon,
+} from "@heroicons/react/solid";
+
+const Main = () => {
+  return (
+    <main
+      className="flex flex-col vsm:border-x border-solid border-outline "
+      id="main"
+    >
+      <header className="z-10 sticky top-0 bg-primary flex items-center text-left pt-2 pr-0 pb-2 pl-3 border-b border-solid border-outline">
+        <button className=" p-2 rounded-[50%] outline-0	cursor-pointer hover:bg-twitterDarkHover ">
+          <ArrowLeftIcon className="icon" />
+        </button>
+
+        <div className="ml-4 flex flex-col">
+          <strong className="text-xl">Micael Souza</strong>
+          <span className="text-base text-gray">69 Tweets</span>
+        </div>
+      </header>
+
+      <div id="profile-page"></div>
+
+      <div
+        className="fixed bottom-0 left-0 z-10 bg-primary w-full border-t border-solid border-outline flex justify-between vsm:hidden"
+        id="bottomMenu"
+      >
+        <HomeIcon className="icon" />
+        <SearchIcon className="icon" />
+        <BellIcon className="icon" />
+        <MailIcon className="icon" />
+      </div>
+    </main>
+  );
+};
+
+export default Main;
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+        search: "var(--search)",
+        white: "var(--white)",
+        gray: "var(--gray)",
+        outline: "var(--outline)",
+        retweet: "var(--retweet)",
+        like: "var(--like)",
+        twitter: "var(--twitter)",
+        twitterDarkHover: "var(--twitter-dark-hover)",
+        twitterLightHover: "var(--twitter-light-hover)",
+      },
+      screens: {
+        'vsm': '500px'
+      },
+    },
+  },
+  plugins: [],
+}
